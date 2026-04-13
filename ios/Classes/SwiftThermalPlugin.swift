fb73d41263cf4c84b87ac720a53d24dade5d7271
王洋(Wang Yang/比克) <Cris@olaparty.sg>
2026-04-13 12:00:12 +0800

    feat：ui scene适配

--- ios/Classes/SwiftThermalPlugin.swift
+++ ios/Classes/SwiftThermalPlugin.swift
@@ -2,12 +2,11 @@ import Flutter
 import UIKit
 
 @available(iOS 11.0, *)
-public class SwiftThermalPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+public class SwiftThermalPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, FlutterSceneLifeCycleDelegate {
   var sink: FlutterEventSink?
   @objc public static var canSendMsg = false
-    @objc public static var isForeground = false
+  @objc public static var isForeground = false
 
-  
   public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
     self.sink = events
     NotificationCenter.default.addObserver(
@@ -17,13 +16,13 @@ public class SwiftThermalPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
       object: nil)
     return nil
   }
-  
+
   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
     self.sink = nil
     NotificationCenter.default.removeObserver(self, name: ProcessInfo.thermalStateDidChangeNotification, object: nil)
     return nil
   }
-  
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch (call.method) {
     case "getThermalStatus":
@@ -33,7 +32,7 @@ public class SwiftThermalPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
       result(FlutterMethodNotImplemented)
     }
   }
-  
+
   private static func toChannelValue(state: ProcessInfo.ThermalState) -> Int {
     switch state {
     case ProcessInfo.ThermalState.nominal:
@@ -48,33 +47,66 @@ public class SwiftThermalPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
       return 0
     }
   }
-  
+
+  private func updateForegroundState(_ isForeground: Bool) {
+    SwiftThermalPlugin.isForeground = isForeground
+  }
+
   @objc public func onThermalStateChanged() {
     if let events = self.sink, SwiftThermalPlugin.canSendMsg, SwiftThermalPlugin.isForeground {
-        events(SwiftThermalPlugin.toChannelValue(state: ProcessInfo.processInfo.thermalState))
+      events(SwiftThermalPlugin.toChannelValue(state: ProcessInfo.processInfo.thermalState))
     }
   }
-  
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let eventChannel = FlutterEventChannel(name: "thermal/events", binaryMessenger: registrar.messenger())
     let methodChannel = FlutterMethodChannel(name: "thermal", binaryMessenger: registrar.messenger())
     let instance = SwiftThermalPlugin()
     eventChannel.setStreamHandler(instance)
     registrar.addMethodCallDelegate(instance, channel: methodChannel)
-      registrar.addApplicationDelegate(instance)
+    registrar.addApplicationDelegate(instance)
+    if #available(iOS 13.0, *) {
+      registrar.addSceneDelegate(instance)
+    }
   }
-    
-    // MARK: - ApplicationDelegate
-    
-    public func applicationDidBecomeActive(_ application: UIApplication) {
-        SwiftThermalPlugin.isForeground = true
-    }
-    
-    public func applicationWillEnterForeground(_ application: UIApplication) {
-        SwiftThermalPlugin.isForeground = true
-    }
-    
-    public func applicationDidEnterBackground(_ application: UIApplication) {
-        SwiftThermalPlugin.isForeground = false
-    }
+
+  // MARK: - ApplicationDelegate
+
+  public func applicationDidBecomeActive(_ application: UIApplication) {
+    updateForegroundState(true)
+  }
+
+  public func applicationWillEnterForeground(_ application: UIApplication) {
+    updateForegroundState(true)
+  }
+
+  public func applicationWillResignActive(_ application: UIApplication) {
+    updateForegroundState(false)
+  }
+
+  public func applicationDidEnterBackground(_ application: UIApplication) {
+    updateForegroundState(false)
+  }
+
+  // MARK: - SceneDelegate
+
+  @available(iOS 13.0, *)
+  public func sceneDidBecomeActive(_ scene: UIScene) {
+    updateForegroundState(true)
+  }
+
+  @available(iOS 13.0, *)
+  public func sceneWillEnterForeground(_ scene: UIScene) {
+    updateForegroundState(true)
+  }
+
+  @available(iOS 13.0, *)
+  public func sceneWillResignActive(_ scene: UIScene) {
+    updateForegroundState(false)
+  }
+
+  @available(iOS 13.0, *)
+  public func sceneDidEnterBackground(_ scene: UIScene) {
+    updateForegroundState(false)
+  }
 }
